f14e38d0df8a5cb71c1654325d2297490da2f998
Marco Floriano <marcofloriano@gmail.com>
2024-08-14 13:57:41 -0300

    Second commit

--- User.php
+++ User.php
@@ -5,9 +5,13 @@ namespace Acme;
 class User 
 {
 	protected $loggedIn = false;
-	protected $data = [];
+	protected $data = [
+		'username' => 'marcofloriano',
+		'password' => 'tomate',
+		'lastSeen' => '2024-08-14 13:49:20',
+	];
 
-	public function getData()
+	public function getData($foo)
 	{
 		return $this->data;
 	}
--- index.php
+++ index.php
@@ -2,19 +2,30 @@
 
 include 'User.php';
 
-// $user = new User;
+$user = new Acme\User;
 
 // if($user->isLoggedIn()) {
 // 	echo 'You are logged in';
 // }
 
-$rc = new ReflectionClass('Acme\User');
+// $rc = new ReflectionClass('Acme\User');
+// $rm = new ReflectionMethod('Acme\User', 'getData');
+$rp = new ReflectionProperty('Acme\User', 'data');
+$rp->setAccessible(true);
 
 // echo '<pre>' . print_r(get_class_methods($rc), true);
+// echo '<pre>' . print_r(get_class_methods($rm), true);
+echo '<pre>';
+print_r($rp->getValue($user));
+// echo '<pre>' . print_r(get_class_methods($rp), true);
+// echo $rm->isConstructor();
+// $user = new Acme\User;
+// $foobar = $rm->invokeArgs($user, ['hello world']);
+// echo $foobar;
 
 // if($rc->hasMethod('isLoggedIn'))
 // 	echo 'Has method isLoggedIn';
 
-echo $rc->getName();
+// echo $rc->getShortName();
 
 ?>
